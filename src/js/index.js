1902de017c7946197d8effe95795cef6fd01998d
MargoBil <margo10011990@gmail.com>
2020-04-25 19:35:17 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ const refs = {
   listOfImages: document.querySelector ('.list__images'),
   input: document.querySelector ('input[name="query"]'),
   button: document.querySelector ('.button-loader'),
+  body: document.querySelector ('body'),
 };
 
 let numberOfPage = 1;
@@ -19,33 +20,34 @@ refs.button.addEventListener ('click', handlerNexPagesOfImages);
 function renderListOfImages () {
   getApiRequest (keyWord, numberOfPage, userKey).then (data => {
     const markup = templateListImages (data);
-    const scroll = document.documentElement.clientHeight + document.documentElement.scrollTop;
     refs.listOfImages.insertAdjacentHTML ('beforeend', markup);
-    window.scrollTo ({
-      top: scroll,
-      behavior: 'smooth',
-    });
   });
 }
 
 function handlerSearchingImages (event) {
   if (event.data === '' || event.data === null) {
     refs.button.classList.remove ('visible');
-    refs.listOfImages.innerHTML = '';
     return;
-  };
+  }
   refs.button.classList.remove ('visible');
-    refs.listOfImages.innerHTML = '';
-    numberOfPage = 1;
+  refs.listOfImages.innerHTML = '';
+  numberOfPage = 1;
   keyWord = event.target.value;
   renderListOfImages ();
-  setTimeout(() => {
+  setTimeout (() => {
     refs.button.classList.add ('visible');
   }, 2000);
 }
 
 function handlerNexPagesOfImages (event) {
   event.preventDefault ();
+  let scroll = document.documentElement.offsetHeight;
   numberOfPage += 1;
   renderListOfImages ();
+  setTimeout (() => {
+    window.scrollTo ({
+      top: scroll,
+      behavior: 'smooth',
+    });
+  }, 1000);
 }
